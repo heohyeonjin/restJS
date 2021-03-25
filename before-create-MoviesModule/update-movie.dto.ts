c846cbe68ad5aa6ead70a2d0dd16375fbaa9390e
heohyeonjin <72252806+heohyeonjin@users.noreply.github.com>
2021-03-25 15:24:38 +0900

    Create update-movie.dto.ts

---
+++ before-create-MoviesModule/update-movie.dto.ts
@@ -0,0 +1,6 @@
+/*dto: 데이터 전송 객체*/
+import { IsNumber, IsString } from 'class-validator';
+import { PartialType } from "@nestjs/mapped-types";
+import {CreateMovieDto} from './create-movie.dto';
+export class UpdateMovieDto extends PartialType(CreateMovieDto){}
+
